dc8a1d8f6daf88c6ca33373b20fc6c6b3098f2c8
Nestor Mamani <nmamanipantoja@gmail.com>
2022-09-30 22:21:33 -0500

    feat: getRecomendedProfiles service

---
+++ src/services/profiles/getRecommendedProfiles.ts
@@ -0,0 +1,99 @@
+import { gql } from '@apollo/client';
+import client from '@/services/apollo-client';
+
+import { Profile } from '@/types/profile';
+
+export interface RecommendedProfilesQueryResponse {
+  recommendedProfiles: Array<Profile>;
+}
+
+export const getRecommendedProfiles = async () => {
+  const { data } = await client.query<RecommendedProfilesQueryResponse>({
+    query: gql`
+      query RecommendedProfiles {
+        recommendedProfiles {
+          id
+          name
+          bio
+          attributes {
+            displayType
+            traitType
+            key
+            value
+          }
+          followNftAddress
+          metadata
+          isDefault
+          picture {
+            ... on NftImage {
+              contractAddress
+              tokenId
+              uri
+              verified
+            }
+            ... on MediaSet {
+              original {
+                url
+                mimeType
+              }
+            }
+            __typename
+          }
+          handle
+          coverPicture {
+            ... on NftImage {
+              contractAddress
+              tokenId
+              uri
+              verified
+            }
+            ... on MediaSet {
+              original {
+                url
+                mimeType
+              }
+            }
+            __typename
+          }
+          ownedBy
+          dispatcher {
+            address
+            canUseRelay
+          }
+          stats {
+            totalFollowers
+            totalFollowing
+            totalPosts
+            totalComments
+            totalMirrors
+            totalPublications
+            totalCollects
+          }
+          followModule {
+            ... on FeeFollowModuleSettings {
+              type
+              amount {
+                asset {
+                  symbol
+                  name
+                  decimals
+                  address
+                }
+                value
+              }
+              recipient
+            }
+            ... on ProfileFollowModuleSettings {
+              type
+            }
+            ... on RevertFollowModuleSettings {
+              type
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  return data.recommendedProfiles;
+};
---
+++ src/types/profile.ts
@@ -0,0 +1,43 @@
+export interface Profile {
+  id: string;
+  name: string;
+  bio: string;
+  attributes: Attribute[];
+  followNftAddress: string;
+  metadata: string;
+  isDefault: boolean;
+  picture: Picture;
+  handle: string;
+  coverPicture: Picture;
+  ownedBy: string;
+  dispatcher: null;
+  stats: Stats;
+  followModule: null;
+}
+
+export interface Attribute {
+  displayType: null;
+  traitType: string;
+  key: string;
+  value: string;
+}
+
+export interface Picture {
+  original: Original;
+  __typename: string;
+}
+
+export interface Original {
+  url: string;
+  mimeType: null;
+}
+
+export interface Stats {
+  totalFollowers: number;
+  totalFollowing: number;
+  totalPosts: number;
+  totalComments: number;
+  totalMirrors: number;
+  totalPublications: number;
+  totalCollects: number;
+}
