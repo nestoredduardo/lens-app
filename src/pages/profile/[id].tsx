ed1c9286e735c044cf86d42bfa17f8a362ace4b3
Nestor Mamani <nmamanipantoja@gmail.com>
2022-10-05 21:06:04 -0500

    feat: Show user info in profile page

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -6,6 +6,7 @@ import {
 } from 'next';
 import { ParsedUrlQuery } from 'querystring';
 import { Profile } from '@/types/profile';
+import { ServerError } from '@/types/services';
 
 // Services
 import { getProfile } from '@/services/profiles';
@@ -26,6 +27,12 @@ export const getServerSideProps: GetServerSideProps<
 
   const profile = await getProfile(id as string);
 
+  if (profile instanceof ServerError) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: {
       profile,
@@ -36,9 +43,38 @@ export const getServerSideProps: GetServerSideProps<
 const Profile: NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ profile }) => {
-  console.log(profile);
-
-  return <div>Profile</div>;
+  return (
+    <div>
+      {profile.picture?.original ? (
+        <img
+          src={profile.picture.original.url}
+          alt={profile.name}
+          className="object-cover w-16 h-16 rounded-md"
+        />
+      ) : (
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-16 h-16 text-gray-300"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </div>
+      )}
+      <h2>{profile.handle}</h2>
+      <p>{profile.bio}</p>
+      <p>Followers: {profile.stats.totalFollowers}</p>
+      <p>Following: {profile.stats.totalFollowing}</p>
+    </div>
+  );
 };
 
 export default Profile;
--- src/services/profiles/getProfile.ts
+++ src/services/profiles/getProfile.ts
@@ -2,98 +2,105 @@ import { gql } from '@apollo/client';
 import client from '@/services/apollo-client';
 
 import { Profile } from '@/types/profile';
+import { ServerError } from '@/types/services';
 
 export interface ProfileQueryResponse {
   profile: Profile;
 }
 
-export const getProfile = async (id: string): Promise<Profile> => {
-  const { data } = await client.query<ProfileQueryResponse>({
-    query: gql`
-      query Profile {
-        profile(request: { profileId: "${id}" }) {
-          id
-          name
-          bio
-          attributes {
-            displayType
-            traitType
-            key
-            value
-          }
-          followNftAddress
-          metadata
-          isDefault
-          picture {
-            ... on NftImage {
-              contractAddress
-              tokenId
-              uri
-              verified
+export const getProfile = async (
+  id: string
+): Promise<Profile | ServerError> => {
+  try {
+    const { data } = await client.query<ProfileQueryResponse>({
+      query: gql`
+        query Profile {
+          profile(request: { profileId: "${id}" }) {
+            id
+            name
+            bio
+            attributes {
+              displayType
+              traitType
+              key
+              value
             }
-            ... on MediaSet {
-              original {
-                url
-                mimeType
+            followNftAddress
+            metadata
+            isDefault
+            picture {
+              ... on NftImage {
+                contractAddress
+                tokenId
+                uri
+                verified
               }
-            }
-            __typename
-          }
-          handle
-          coverPicture {
-            ... on NftImage {
-              contractAddress
-              tokenId
-              uri
-              verified
-            }
-            ... on MediaSet {
-              original {
-                url
-                mimeType
-              }
-            }
-            __typename
-          }
-          ownedBy
-          dispatcher {
-            address
-            canUseRelay
-          }
-          stats {
-            totalFollowers
-            totalFollowing
-            totalPosts
-            totalComments
-            totalMirrors
-            totalPublications
-            totalCollects
-          }
-          followModule {
-            ... on FeeFollowModuleSettings {
-              type
-              amount {
-                asset {
-                  symbol
-                  name
-                  decimals
-                  address
+              ... on MediaSet {
+                original {
+                  url
+                  mimeType
                 }
-                value
               }
-              recipient
+              __typename
             }
-            ... on ProfileFollowModuleSettings {
-              type
+            handle
+            coverPicture {
+              ... on NftImage {
+                contractAddress
+                tokenId
+                uri
+                verified
+              }
+              ... on MediaSet {
+                original {
+                  url
+                  mimeType
+                }
+              }
+              __typename
             }
-            ... on RevertFollowModuleSettings {
-              type
+            ownedBy
+            dispatcher {
+              address
+              canUseRelay
+            }
+            stats {
+              totalFollowers
+              totalFollowing
+              totalPosts
+              totalComments
+              totalMirrors
+              totalPublications
+              totalCollects
+            }
+            followModule {
+              ... on FeeFollowModuleSettings {
+                type
+                amount {
+                  asset {
+                    symbol
+                    name
+                    decimals
+                    address
+                  }
+                  value
+                }
+                recipient
+              }
+              ... on ProfileFollowModuleSettings {
+                type
+              }
+              ... on RevertFollowModuleSettings {
+                type
+              }
             }
           }
         }
-      }
-    `,
-  });
+      `,
+    });
 
-  return data.profile;
+    return data.profile;
+  } catch (error) {
+    return new ServerError('Failed to fetch profile');
+  }
 };
